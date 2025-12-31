e0d19d460521dce593c7d12283104e3ae83931ee
minorun365 <mail@minoruonda.com>
2025-12-31 16:47:47 +0900

    更新

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,22 @@ import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+// 追加
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+
+    {/* 追加 */}
+    <Authenticator>
+
+      <App />
+
+    {/* 追加 */}
+    </Authenticator>
+
   </React.StrictMode>
 );
